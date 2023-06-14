357fda98334f42803b1a983d71fdeec74d220156
Nikolay Sergeev <nick>
2023-06-14 17:33:06 +0200

    #2 set application path as environment

--- docker/up.sh
+++ docker/up.sh
@@ -1,3 +1,3 @@
 cd ./../
 docker build -t gersvit -f docker/Dockerfile .
-docker run -v $(pwd):/app -d gersvit /bin/bash | tee docker/container.id
+docker run --env-file $(pwd)/docker/.env -v $(pwd):/app -d gersvit /bin/bash | tee docker/container.id
